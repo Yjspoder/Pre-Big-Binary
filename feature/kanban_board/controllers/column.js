2734c89ca37a04ddd8542d1b54dce67af63415ef
yjspoder <injosse.joshi@gmail.com>
2020-02-21 03:35:16 +0530

    add note crud

--- feature/kanban_board/controllers/column.js
+++ feature/kanban_board/controllers/column.js
@@ -3,22 +3,22 @@ const Column = require('../models/column');
 module.exports = {
     list: (req , res) => {
         Column.find({} , (err , columnList) => {
-            if(err) res.status(500).json(err)
-            return res.status(200).json({columnList})
+            if(err) res.status(500).json(err);
+            return res.status(200).json({columnList});
         })
     },
     create: (req , res) => {
         const title = req.body.title;
         Column.create({ title } , (err , createdColumn) => {
             if (err) return res.status(500).json(err);
-            return res.json({createdColumn})
+            return res.json({createdColumn});
         })
     },
     delete: (req , res) => {
         const id = req.params.id;
         Column.findByIdAndDelete(id , (err , deletedColumn) => {
             if (err) return res.status(500).json(err);
-            return res.status(200).json({deletedColumn})
+            return res.status(200).json({deletedColumn});
         })
     }
 }
